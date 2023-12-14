aaadd161895da26ec002ef57b3c7227fd2458b34
ganesh mahajan <ganesh@logitax.in>
2023-12-14 10:08:43 +0530

    solution added

---
+++ PrintPrimeNumbers.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class PrintPrimeNumbers {
+
+  public static void main (String[] args) {
+    int n = 100;
+    
+    for(int i=2; i<=n; i++){
+      boolean isPrime = true;
+      
+      // for(int j=2; j*j<=i; j++){
+      //   if(i%j==0){
+      //     isPrime=false;
+      //     break;
+      //   }
+      // }
+      
+      for(int j=2; j<i; j++){
+        if(i%j==0){
+          isPrime=false;
+          break;
+        }
+      }
+      
+      if(isPrime)
+      System.out.print(i+" ");
+    }
+    
+  }
+
+}
