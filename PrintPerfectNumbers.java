59651ad4daf1f5c010c6b148ca7d9afa86f61195
ganesh mahajan <ganesh@logitax.in>
2023-12-16 10:20:13 +0530

    added

---
+++ PrintPerfectNumbers.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class PrintPerfectNumbers {
+  
+    static boolean isPerfect(int n){
+      if(n<=1)
+        return false;
+        
+      int sum=1;
+      
+      for(int i=2; i*i<=n; i++){
+        if(n%i==0){
+          sum+=i;
+          
+          if(i*i!=n)
+            sum+=n/i;
+        }
+      }
+      
+      return sum==n;
+    }
+    public static void main(String[] args) {
+      int n = 100;
+      for(int i=2; i<=n; i++){
+        if(isPerfect(i)){
+          System.out.print(i+" ");
+        }
+      }
+      
+  }
+}
